c77a946b1fc940fc6d475646ae5926b90c8376ad
Neradoc <neraOnGit@ri1.fr>
2023-04-20 05:22:15 +0200

    working version, with example

--- TM1637Display.py
+++ TM1637Display.py
@@ -1,4 +1,4 @@
-'''
+"""
 //  Author: avishorp@gmail.com
 //
 //  This library is free software; you can redistribute it and/or
@@ -14,9 +14,15 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-'''
+"""
 import digitalio
 import microcontroller
+
+try:
+    from typing import Union, List, Tuple, Optional, Dict
+    from microcontroller import Pin
+except ImportError:
+    pass
 
 SEG_A = 0b00000001
 SEG_B = 0b00000010
@@ -25,7 +31,7 @@ SEG_D = 0b00001000
 SEG_E = 0b00010000
 SEG_F = 0b00100000
 SEG_G = 0b01000000
-SEG_DP= 0b10000000
+SEG_DP = 0b10000000
 
 DEFAULT_BIT_DELAY = 100
 
@@ -33,7 +39,7 @@ TM1637_I2C_COMM1 = 0x40
 TM1637_I2C_COMM2 = 0xC0
 TM1637_I2C_COMM3 = 0x80
 
-'''
+"""
 //
 //      A
 //     ---
@@ -42,192 +48,263 @@ TM1637_I2C_COMM3 = 0x80
 //  E |   | C
 //     ---
 //      D
-'''
-digitToSegment = [
-# XGFEDCBA
-  0b00111111,    # 0
-  0b00000110,    # 1
-  0b01011011,    # 2
-  0b01001111,    # 3
-  0b01100110,    # 4
-  0b01101101,    # 5
-  0b01111101,    # 6
-  0b00000111,    # 7
-  0b01111111,    # 8
-  0b01101111,    # 9
-  0b01110111,    # A
-  0b01111100,    # b
-  0b00111001,    # C
-  0b01011110,    # d
-  0b01111001,    # E
-  0b01110001     # F
+"""
+digit_to_segment = [
+    # XGFEDCBA
+    0b00111111,  # 0
+    0b00000110,  # 1
+    0b01011011,  # 2
+    0b01001111,  # 3
+    0b01100110,  # 4
+    0b01101101,  # 5
+    0b01111101,  # 6
+    0b00000111,  # 7
+    0b01111111,  # 8
+    0b01101111,  # 9
+    0b01110111,  # A
+    0b01111100,  # b
+    0b00111001,  # C
+    0b01011110,  # d
+    0b01111001,  # E
+    0b01110001,  # F
 ]
+minus_segments = 0b01000000
+letter_to_segment = {
+    letter: digit_to_segment[pos] for pos, letter in enumerate("0123456789abcdef")
+}
+letter_to_segment.update(
+    {
+        "": 0,
+        " ": 0,
+        "-": minus_segments,
+    }
+)
 
-minusSegments = 0b01000000
 
 class TM1637Display:
-    def __init__(self, pinClk, pinDIO, bitDelay=DEFAULT_BIT_DELAY):
-        # Copy the pin numbers
-        self.Clk = digitalio.DigitalInOut(pinClk)
-        self.DIO = digitalio.DigitalInOut(pinDIO)
-        self._bitDelay = bitDelay
-        self.brightness = 0x0F
+    """
+    Numeric 7-segment display.
+
+    :param microcontroller.Pin clock: The clock pin.
+    :param microcontroller.Pin data: The data pin.
+    :param int length: The number of characters on the display.
+    :param int digit_order: The order of the digits on the display, if it doesn't
+        match the internal order of the chip. Reverse it for LTR.
+    :param bool auto_write: True if the display should immediately change when set.
+        If False, `show` must be called explicitly.
+    :param int bit_delay: the (minimum) delay between bit twiddlings in us.
+    """
+
+    def __init__(
+        self,
+        clock: Pin,
+        data: Pin,
+        length: int = 4,
+        digit_order: tuple = None,
+        auto_write: bool = True,
+        bit_delay: int = DEFAULT_BIT_DELAY,
+    ):
+        if not digit_order:
+            self.digit_order = (2, 1, 0, 5, 4, 3)
+        else:
+            if len(digit_order) != length:
+                raise ValueError("digit_order is a list of all digit positions")
+            self.digit_order = digit_order
+
+        self._bit_delay = bit_delay
+        self._auto_write = auto_write
+        self.brightness = 7
+        self.digits = bytearray(length)
 
         # Set the pin direction and default value.
-        # Both pins are set as inputs, allowing the pull-up resistors to pull them up
+        self.Clk = digitalio.DigitalInOut(clock)
+        self.DIO = digitalio.DigitalInOut(data)
         self.Clk.switch_to_output(True)
         self.DIO.switch_to_output(True)
 
-    def setBrightness(self, brightness, on=True):
-        self.brightness = (brightness & 0x7) | ( 0x08 if on else 0x00)
+    def _set_brightness(self, brightness: int, on: bool = True):
+        """Set the brightness from 0 to 7, and enable light or not"""
+        self._brightness = (brightness & 0x7) | (0x08 if on else 0x00)
 
-    def setSegments(self, segments, length=0, pos=0):
-        length = length or len(segments)
-        self.start()
-        self.writeByte(TM1637_I2C_COMM1)
-        self.stop()
+    @property
+    def brightness(self):
+        """Brightness is a value between 0 and 7. Set to None to turn off."""
+        if self._brightness & 0x8:
+            return False
+        return self._brightness & 0x7
+
+    @brightness.setter
+    def brightness(self, value: int):
+        if value is None:
+            self._set_brightness(0, on=False)
+        elif value not in range(8):
+            raise ValueError("brightness must be in the range 0-7")
+        else:
+            self._set_brightness(value)
+
+    def set_segments(self, segments: bytes, pos=0):
+        """Directly set the bit values of the segments"""
+        self._start()
+        self._write_byte(TM1637_I2C_COMM1)
+        self._stop()
 
         # Write COMM2 + first digit address
-        self.start()
-        self.writeByte(TM1637_I2C_COMM2 + (pos & 0x03))
+        self._start()
+        self._write_byte(TM1637_I2C_COMM2)
 
         # Write the data bytes
-        for k in range(length):
-            self.writeByte(segments[k])
-
-        self.stop()
+        length = len(self.digit_order)
+        digit_order = self.digit_order
+        for offset in range(length):
+            k = digit_order[(pos + offset) % length]
+            self._write_byte(segments[k])
+        self._stop()
 
         # Write COMM3 + brightness
-        self.start()
-        self.writeByte(TM1637_I2C_COMM3 + (self.brightness & 0x0f))
-        self.stop()
+        self._start()
+        self._write_byte(TM1637_I2C_COMM3 + (self._brightness & 0x0F))
+        self._stop()
 
     def clear(self):
-        self.setSegments((0, 0, 0, 0))
+        self.set_segments(bytearray(len(self.digits)))
 
-    def showNumberDec(self, num, leading_zero=False, length=4, pos=0):
-        self.showNumberDecEx(num, 0, leading_zero, length, pos)
+    def bit_delay(self):
+        microcontroller.delay_us(self._bit_delay)
 
-    def showNumberDecEx(self, num, dots=0, leading_zero=False, length=4, pos=0):
-        self.showNumberBaseEx(
-            -10 if num < 0 else 10,
-            -num if num < 0 else num,
-            dots,
-            leading_zero,
-            length,
-            pos
-        )
+    def _start(self):
+        self.DIO.value = False  # ?
+        self.bit_delay()
 
-    def showNumberHexEx(self, num, dots=0, leading_zero=False, length=4, pos=0):
-        self.showNumberBaseEx(16, num, dots, leading_zero, length, pos)
-
-    def showNumberBaseEx(self, base, num, dots=0, leading_zero=False, length=4, pos=0):
-        negative = False
-        if base < 0:
-            base = -base
-            negative = True
-
-        digits = bytearray(4)
-
-        if num == 0 and not leading_zero:
-            # Singular case - take care separately
-            for i in range(length - 1):
-                digits[i] = 0
-            digits[length - 1] = self.encodeDigit(0)
-
-        else:
-            #uint8_t i = length-1
-            #if (negative) {
-            #	# Negative number, show the minus sign
-            #    digits[i] = minusSegments
-            #	i--
-            #}
-            for i in range(length-1, -1, -1):
-                digit = num % base
-
-                if digit == 0 and num == 0 and leading_zero == False:
-                    # Leading zero is blank
-                    digits[i] = 0
-                else:
-                    digits[i] = self.encodeDigit(digit)
-
-                if digit == 0 and num == 0 and negative:
-                    digits[i] = minusSegments
-                    negative = False
-
-                num //= base
-
-        if dots != 0:
-            self.showDots(dots, digits)
-        self.setSegments(digits, length, pos)
-
-
-    def bitDelay(self):
-        microcontroller.delay_us(self._bitDelay)
-
-    def start(self):
-        self.DIO.value = True # ?
-        microcontroller.delay_us(self._bitDelay)
-
-    def stop(self):
-        self.DIO.value = False # ?
-        microcontroller.delay_us(self._bitDelay)
+    def _stop(self):
+        self.DIO.value = False  # ?
+        self.bit_delay()
         self.Clk.value = True
-        microcontroller.delay_us(self._bitDelay)
+        self.bit_delay()
         self.DIO.value = True
-        microcontroller.delay_us(self._bitDelay)
+        self.bit_delay()
 
-    def writeByte(self, byte):
-        data = byte
-
+    def _write_byte(self, data: int):
+        """Write a byte to the board, bit bangging... is it I2C ?"""
         # 8 Data Bits
         for i in range(8):
             # CLK low
             self.Clk.value = False
-            microcontroller.delay_us(self._bitDelay)
+            self.bit_delay()
 
             # Set data bit
             if data & 0x01:
                 self.DIO.value = True
             else:
-                self.DIO.value = False # ?
+                self.DIO.value = False  # ?
 
-            microcontroller.delay_us(self._bitDelay)
+            self.bit_delay()
 
             # CLK high
             self.Clk.value = True
-            microcontroller.delay_us(self._bitDelay)
+            self.bit_delay()
             data = data >> 1
 
         # Wait for acknowledge
         # CLK to zero
         self.Clk.value = False
         self.DIO.switch_to_input(digitalio.Pull.UP)
-        microcontroller.delay_us(self._bitDelay)
+        self.bit_delay()
 
         # CLK to high
         self.Clk.value = True
-        microcontroller.delay_us(self._bitDelay)
+        self.bit_delay()
         ack = self.DIO.value
         if ack is False:
-            print("ack")
-            self.DIO.switch_to_output(False) #?
+            self.DIO.switch_to_output(False)
         else:
-            print("noack")
+            # ?? do nothing ? raise ? (switch to output anyway)
             self.DIO.switch_to_output(True)
 
-
-        microcontroller.delay_us(self._bitDelay)
+        self.bit_delay()
         self.Clk.value = False
-        microcontroller.delay_us(self._bitDelay)
+        self.bit_delay()
 
         return ack
 
-    def showDots(self, dots, digits):
-        for i in range(4):
-            digits[i] |= (dots & 0x80)
-            dots <<= 1
-        return digits
+    def show_dots(self, dots: tuple):
+        """Set the locations of dots on the display."""
+        for i, num in enumerate(self.digits):
+            if dots[i]:
+                self.digits[i] |= 0x80
+            else:
+                self.digits[i] &= 0x7F
+        if self._auto_write:
+            self.show()
 
-    def encodeDigit(self, digit):
-        return digitToSegment[digit & 0x0f]
+    def show(self):
+        """Refresh the display and show the changes."""
+        self.set_segments(self.digits)
+
+    @property
+    def auto_write(self) -> bool:
+        """Auto write updates to the display."""
+        return self._auto_write
+
+    @auto_write.setter
+    def auto_write(self, auto_write: bool) -> None:
+        self._auto_write = bool(auto_write)
+
+    def print(self, value: Union(int, float, str), decimal: int = 0):
+        """Print the value to the display.
+
+        :param str|float value: The value to print
+        :param int decimal: The number of decimal places for a floating point
+            number if decimal is greater than zero, or the input number is an
+            integer if decimal is zero.
+        """
+        if isinstance(value, str):
+            self._text(value)
+        elif isinstance(value, (int, float)):
+            self._number(value, decimal)
+        else:
+            raise ValueError("Unsupported display value type: {}".format(type(value)))
+        if self._auto_write:
+            self.show()
+
+    def print_hex(self, value: Union(int, float, str)):
+        """Print the value as a hexidecimal string to the display.
+
+        :param int|str value: The number to print
+        """
+        if isinstance(value, int):
+            self.print("{0:x}".format(value))
+        else:
+            self.print(value)
+
+    def _text(self, text: str) -> None:
+        """Display the specified text."""
+        length = len(self.digits)
+        k = length - 1
+        dot = False
+        for letter in reversed(text):
+            if letter == ".":
+                dot = True
+                continue
+            self.digits[k] = letter_to_segment[letter.lower()]
+            if dot:
+                self.digits[k] |= SEG_DP
+                dot = False
+            k = k - 1
+            if k < 0:
+                break
+        for i in range(k + 1):
+            self.digits[i] = 0
+
+    def _number(self, number: Union(float, int), decimal: int = 0) -> str:
+        """Format and siplay a number"""
+        if isinstance(number, float) and decimal > 0:
+            stnum = f"{number:f}"
+        else:
+            stnum = str(int(number)) + "."
+        dot = stnum.find(".")
+        # cut the decimals
+        if dot >= 0:
+            stnum = stnum[: dot + decimal + 1]
+            stnum = stnum[: len(self.digits) + 1]
+        # make it a text
+        self._text(stnum)
